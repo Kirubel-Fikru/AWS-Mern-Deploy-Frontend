84a27f12e58d05b5f98e81ce8c90646f6527b353
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-07-06 14:04:18 +0300

    Ansible Test7

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://ec2-54-159-208-31.compute-1.amazonaws.com:8000/api",
 })
 export default instance
 //test  k
