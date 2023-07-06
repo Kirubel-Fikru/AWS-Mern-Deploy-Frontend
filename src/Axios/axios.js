18680427abf4cea773eff6f7ed84fe81f7093a46
Kirubel-Fikru <kirubelfikruwsh@gmail.com>
2023-07-06 13:47:21 +0300

    Frontend ansible4

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-  baseURL: "http://ec2-54-159-208-31.compute-1.amazonaws.com:8000/api",
+  baseURL: "http://172.31.87.92:8000/api",
 })
 export default instance
 //test  k
